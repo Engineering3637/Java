c53be9b0449de9f7b10488e7d553a98213634b12
Michael Awosemo <MAwosemo@spartaglobal.com>
2019-08-28 14:59:28 +0100

    Add generated cookbook content

---
+++ files/java.sh
@@ -0,0 +1,7 @@
+
+sudo apt-get update
+sudo add-apt-repository ppa:webupd8team/java
+
+sudo apt-get install default-jre -y
+sudo apt-get install default-jdk -y
+sudo apt-get install oracle-java8-installer -y
